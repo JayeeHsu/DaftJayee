e9b7439dfc136b5c3752bc545c40a00086f25f1c
xujiayi03 <xujiayi03@kuaishou.com>
2023-04-13 17:14:12 +0800

    feat: NavMenu

---
+++ src/components/common/NavMenu/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="nav-menu">test</div>
+</template>
+
+<script lang='ts' setup>
+
+</script>
+
+<style scoped>
+.nav-menu{
+    
+}
+</style>
---
+++ src/components/common/index.ts
@@ -0,0 +1,5 @@
+import NavMenu from './NavMenu/index.vue'
+
+export {
+      NavMenu
+}
